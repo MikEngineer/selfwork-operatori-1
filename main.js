9ea18d160fbce701b419b6970c3ef02c882cea7a
Michelangelo Ruisi <mruisi96@gmail.com>
2024-12-20 10:49:21 +0100

    terminato selfwork operatori 1

---
+++ main.js
@@ -0,0 +1,15 @@
+let num1 = 1;
+let num2 = 3;
+let num3 = 5;
+let num4 = 7;
+let num5 = 11;
+
+const somma = (num1 + num2 + num3 + num4 + num5);
+
+console.log(somma);
+
+const media = ((num1 + num2 + num3 + num4 + num5)/5);
+
+console.log(media);
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
